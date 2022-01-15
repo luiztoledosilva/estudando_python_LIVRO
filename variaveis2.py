dfb36f7eb8f921aa8718c36cf0476e0eb03218e7
luiztoledosilva <luiztoledosilva30@gmail.com>
2022-01-15 19:35:09 -0300

    Estudo de variaveis

---
+++ variaveis2.py
@@ -0,0 +1,10 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2*3+1
+7
+(3+1)*3
+12
+4.321 / 3 + 10
+11.440333333333333
+4.321 / (3 + 10)
+0.3323846153846154
