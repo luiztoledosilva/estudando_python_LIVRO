1e09b25d2e0b407e9c722ebfcf56fcc7e773837f
luiztoledosilva <luiztoledosilva30@gmail.com>
2022-01-15 20:03:57 -0300

    arquivo exercicio

---
+++ arquivo_desafio.py
@@ -0,0 +1,11 @@
+###soma dos cinco primeiros numeros positivos
+
+numbers = [1, 2, 3, 4, 5, 1, 4, 5]
+Sum = sum(numbers)
+print(Sum)
+
+###A idade média de Sara (idade 23), Mark (idade 19) e Fátima (idade 31).
+
+sara=23
+mark=19
+fatima=31
