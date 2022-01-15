be86cfa8aa34a814ae6ac9227f5604ae36a29ec4
luiztoledosilva <luiztoledosilva30@gmail.com>
2022-01-15 19:41:34 -0300

    Estudo Exponenciacao

---
+++ funcao_exponenciacao.py
@@ -0,0 +1,17 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2**3
+8
+2**4
+16
+14 // 3
+4
+14 % 3
+2
+abs(-4)
+4
+abs(-3.2)
+3.2
+min(6, -2) //funcao para minino
+SyntaxError: invalid syntax
+
